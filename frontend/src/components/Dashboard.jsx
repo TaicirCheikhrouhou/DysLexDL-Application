509ae36a548e817cc2bb801dc433a16134cdc4bf
TaicirCheikhrouhou <taicir.cheikhrouhou@enis.tn>
2025-11-09 22:37:19 +0100

    Fix import statement

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import Home from './Home'; // Make sure path is correct
-
+import Home from './Home';
 function Dashboard() {
   return (
     <>
       <Navbar />
-      <Home /> {/* Main content (like CTA) goes here */}
+      <Home /> {}
       <Footer />
     </>
   );
